54847760b84bb51891c59b2fa0ed2271a56d0583
Ilya Tsibul <tsigel@vikivostok.ru>
2022-11-03 00:00:10 +0300

    changed db structure foreign key from print_places to print imports Print position connection to print_place & print group

--- maket/models.py
+++ maket/models.py
@@ -18,13 +18,6 @@ class Color_scheme(models.Model):
         return str(self.scheme_name)
 
 
-class Print_place(models.Model):
-    """ detail_name name of part of item
-        print_name name of printing place clip/top/cap"""
-    detail_name = models.CharField(max_length=20)
-    place_name = models.CharField(max_length=30)
-
-
 class Print_group(models.Model):
     """code for similar shapes of items"""
     code = models.CharField(max_length=7, default=0)
@@ -78,10 +71,22 @@ class Print_type(models.Model):
         return str(self.type_name)
 
 
+class Print_place(models.Model):
+    """ detail_name name of part of item
+        print_name name of printing place as in import"""
+    detail_name = models.CharField(max_length=20)
+    place_name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return str(self.detail_name + ' ' + self.place_name)
+
+
 class Print_position(models.Model):
     """ option - open/close pen, plain round
         orientation - left, right, opposite, back"""
-    position_option = models.CharField(max_length=20)
+    position_place = models.ForeignKey(Print_place, models.SET_NULL, null=True)
+    print_group = models.ForeignKey(Print_group, on_delete=models.CASCADE, null=True)
+    orientation_id = models.SmallIntegerField(default=1)
     position_orientation = models.CharField(max_length=20)
 
 
@@ -207,6 +212,7 @@ class Print_imports(models.Model):
     """quantity - number of colors
         shots - number of shots 1 or 2"""
     place = models.CharField(max_length=30, blank=True, null=True)
+    print_place = models.ForeignKey(Print_place, models.SET_NULL, null=True)
     type = models.CharField(max_length=30, blank=True, null=True)
     colors = models.SmallIntegerField(default=1)
     second_pass = models.BooleanField(default=False)
