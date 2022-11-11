b0924583c9d8c33776c49427fc95d8e023abf446
Ilya Tsibul <tsigel@vikivostok.ru>
2022-11-11 11:11:54 +0300

    fixed error in prt_imports tab was moved and lost prt_0 and when prt_0_ maket exists

--- maket/views.py
+++ maket/views.py
@@ -930,10 +930,10 @@ def prt_imports(id, print_import, ord_imp, mk_id):
                         pt_0 = 1
                     else:
                         pt_0 = 0
-                        prt_0_.append([print_item.id, clr_hex, clr, print_item.item.item.print_group.code, pt_0, pt.option, \
-                                       pt_name.replace(' ', '_').replace(',', '').replace('+', '_'), colors_list])
-                        prt_0.append([print_item, clr_hex, print_item.item.item.print_group.code, pt_0, pt.option, \
-                                      pt_name.replace(' ', '_').replace(',', '').replace('+', '_'), colors_list])
+                    prt_0_.append([print_item.id, clr_hex, clr, print_item.item.item.print_group.code, pt_0, pt.option, \
+                                   pt_name.replace(' ', '_').replace(',', '').replace('+', '_'), colors_list])
+                    prt_0.append([print_item, clr_hex, print_item.item.item.print_group.code, pt_0, pt.option, \
+                                  pt_name.replace(' ', '_').replace(',', '').replace('+', '_'), colors_list])
                 except:
                     prt_0_.append([print_item.id, clr_hex, clr, print_item.item.item.print_group.code, 0, 1, \
                                    pt_name.replace(' ', '_').replace(',', '').replace('+', '_'), colors_list])
