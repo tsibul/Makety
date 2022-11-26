2edc285e8007b767058994cb2c47519af1af7fe2
Ilya Tsibul <tsigel@vikivostok.ru>
2022-11-26 19:20:19 +0300

    Error fix for maket_layout

--- maket_layout/urls.py
+++ maket_layout/urls.py
@@ -5,10 +5,10 @@ app_name = 'maket_layout'
 
 urlpatterns = [
 
-    path('maket_print/<int:id>/<int:mk_id>', views.maket_print, name='maket_print'),
+    path('<int:id>/<int:mk_id>', views.maket_print, name='maket_print'),
     path('maket_print_empty/<int:id>/<int:mk_id>', views.maket_print_empty, name='maket_print_empty'),
 
-    path('maket_print/update_maket/<int:id>', views.update_maket, name='update_maket'),
-    path('maket_print/update_maket_empty/<int:id>', views.update_maket_empty, name='update_maket_empty'),
+    path('update_maket/<int:id>', views.update_maket, name='update_maket'),
+    path('update_maket_empty/<int:id>', views.update_maket_empty, name='update_maket_empty'),
 
 ]
