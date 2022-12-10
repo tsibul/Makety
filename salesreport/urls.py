10b17ef271681dac8acecf032e0a4dca5264b60e
Ilya Tsibul <tsigel@vikivostok.ru>
2022-12-10 23:54:57 +0300

    customers, groups, import report, index vviews added

--- salesreport/urls.py
+++ salesreport/urls.py
@@ -5,4 +5,7 @@ app_name = 'salesreport'
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('customers', views.customer_sales, name='customers'),
+    path('groups', views.customer_groups, name='groups'),
+    path('import_report', views.import_report, name='import_report'),
 ]
--- salesreport/views.py
+++ salesreport/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect, reverse
 
 # Create your views here.
+
+def index(request):
+    context = {}
+    return render(request, 'salesreport/index.html', context)
+
+
+def customer_sales(request):
+    context = {}
+    return render(request, 'salesreport/customers.html', context)
+
+
+def customer_groups(request):
+    context = {}
+    return render(request, 'salesreport/groups.html', context)
+
+
+def import_report(request):
+    return HttpResponseRedirect(reverse('salesreport:index'))
