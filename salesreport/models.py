f74e49a334a4b91a848801ac425cbe9b64aae8cd
Ilya Tsibul <tsigel@vikivostok.ru>
2022-12-10 23:54:03 +0300

    customer type, date of first sale to customer, date of import added

--- salesreport/models.py
+++ salesreport/models.py
@@ -4,14 +4,16 @@ from maket.models import Customer, Detail_set, Item_color
 
 class Customer_groups(models.Model):
     group_name = models.CharField(max_length=255)
-    group_type = models.CharField(max_length=20)
+    group_type = models.CharField(max_length=140)
 
 
 class Customers_sales(models.Model):
     customer_id = models.IntegerField
     customer_name = models.CharField(max_length=255)
+    customer_type = models.CharField(max_length=20)
     customer = models.ForeignKey(Customer, models.SET_NULL, null=True)
     customer_group = models.ForeignKey(Customer_groups, models.SET_NULL, null=True)
+    date_first = models.DateField
 
 
 class Sales_docs(models.Model):
@@ -22,6 +24,7 @@ class Sales_docs(models.Model):
 
 
 class Sales_doc_imports(models.Model):
+    import_date = models.DateField
     code = models.CharField(max_length=20)
     detail_set = models.ForeignKey(Detail_set, models.SET_NULL, null=True)
     color_code = models.CharField(max_length=20)
