a0e69da62d9c173567763d825aedaa5ddd800296
Ilya Tsibul <tsigel@vikivostok.ru>
2022-12-30 09:17:12 +0300

    customers all and all customers imports urls

--- salesreport/urls.py
+++ salesreport/urls.py
@@ -6,6 +6,8 @@ app_name = 'salesreport'
 urlpatterns = [
     path('', views.index, name='index'),
     path('customers', views.customer_sales, name='customers'),
+    path('customers_all', views.customer_all, name='customers_all'),
     path('groups', views.customer_groups, name='groups'),
     path('import_report', views.import_report, name='import_report'),
+    path('import_cst', views.import_cst, name='import_cst'),
 ]
