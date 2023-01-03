9ce74dea9c79120369a6c5d336baeb62a2381e48
Ilya Tsibul <tsigel@vikivostok.ru>
2023-01-03 10:15:01 +0300

    Prepare model for import from frigat

--- salesreport/models.py
+++ salesreport/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from maket.models import Customer, Detail_set, Item_color, Order_imports
+from maket.models import Customer, Detail_set, Item_color, Order_imports, Customer_all
 
 
 class Sales_docs(models.Model):
@@ -21,19 +21,44 @@ class Sales_docs(models.Model):
 
 
 class Sales_doc_imports(models.Model):
+    """ Frigat fields from report:
+    series_id
+    good_id
+    good_group_id
+    good_group
+    good_title
+    good_name
+    code_1
+    """
+
     import_date = models.DateField
     code = models.CharField(max_length=20)
     detail_set = models.ForeignKey(Detail_set, models.SET_NULL, null=True)
     color_code = models.CharField(max_length=20)
     item_color = models.ForeignKey(Item_color, models.SET_NULL, null=True)
     series_id = models.IntegerField(null=True)
-    quantity = models.IntegerField
+    good_id = models.IntegerField(null=True)
+    good_group_id = models.IntegerField(null=True)
+    good_group = models.CharField(max_length=255, null=True)
+    good_title = models.CharField(max_length=255, null=True)
+    good_name = models.CharField(max_length=255, null=True)
+    code_1 = models.CharField(max_length=20, null=True)
+    quantity = models.IntegerField(default=0)
+    sales_doc_name = models.CharField(max_length=255, default='Расходная накладная')
+    sales_doc_no = models.IntegerField(null=True)
+    sales_doc_date = models.DateField(default='2010-01-01')
     buy_without_vat = models.FloatField
     buy_with_vat = models.FloatField
     sales_quantity = models.IntegerField
     sale_without_vat = models.FloatField
     sale_with_vat = models.FloatField
     sale_price_vat = models.FloatField
+    customer_name = models.CharField(max_length=255, null=True)
+    customer_frigat_id = models.IntegerField(null=True)
+    customer = models.ForeignKey(Customer, models.SET_NULL, null=True)
+    customer_all = models.ForeignKey(Customer_all, models.SET_NULL, null=True)
+    sales_doc = models.ForeignKey(Sales_docs, models.SET_NULL, null=True)
+
 
     def __repr__(self):
         return self.code
