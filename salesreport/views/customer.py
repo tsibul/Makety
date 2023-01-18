5f3fa0e9517464c3b40d21a6d0a8d78ae04e9e91
Ilya Tsibul <tsigel@vikivostok.ru>
2023-01-18 17:22:38 +0300

    Take of pagination in customers from boottom edit customer added

--- salesreport/views/customer.py
+++ salesreport/views/customer.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render, HttpResponseRedirect, reverse
 from maket.models import Customer_all, Customer, Customer_groups, Customer_types, Detail_set, Item_color
 from django.core.paginator import Paginator
+from Makety.service import *
 
 
 def customer_sales(request):
     navi = 'Клиенты'
     customers = Customer.objects.all().order_by('customer_all__name')
-    paginator = Paginator(customers, 25)  # Show 25 contacts per page.
+    paginator = Paginator(customers, 20)  # Show 20 contacts per page.
 
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
@@ -26,10 +27,66 @@ def customer_sales(request):
 def customer_active(request):
     navi = 'Клиенты'
     customers = Customer.objects.filter(active=True, internal=False).order_by('customer_all__name')
-    paginator = Paginator(customers, 25)  # Show 25 contacts per page.
+    paginator = Paginator(customers, 20)  # Show 25 contacts per page.
 
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
 
     context = {'navi': navi, 'page_obj': page_obj}
     return render(request, 'salesreport/customers.html', context)
+
+
+def update_cst(request):
+    page_no = '?page=' + request.POST['page_no']
+    cst_id = request.POST['id']
+    cst = Customer.objects.get(id=cst_id)
+    nm = request.POST['nm']
+    gr_old = request.POST['gr_o']
+    gr_id = request.POST['gr_id']
+    try:
+        gr = request.POST['gr']
+    except:
+        gr = None
+    tp_old = request.POST['tp_o']
+    rg = request.POST['rg']
+    in_ = request.POST['in_']
+    ad = request.POST['ad']
+    try:
+        tp = request.POST['tp']
+        type = Customer_types.objects.get(id=tp)
+        cst.customer_type = type
+    except:
+        pass
+    if gr != '' or gr_id != '':
+        try:
+            group = Customer_groups.objects.get(id=gr)
+        except:
+            try:
+                group = Customer_groups.objects.get(group_name=gr_old)
+            except:
+                group = Customer_groups(group_name=gr_old)
+                try:
+                    group.group_type = type
+                except:
+                    pass
+                group.save()
+        cst.customer_group = group
+    cst.name = nm
+    cst.group = gr_old
+    cst.type = tp_old
+    cst.region = rg
+    cst.inn = in_
+    cst.address = ad
+    cst.save()
+    cst.customer_all.group = cst.group
+    cst.customer_all.type = cst.type
+    cst.customer_all.customer_group = cst.customer_group
+    cst.customer_all.customer_type = cst.customer_type
+    cst.customer_all.region = cst.region
+    cst.customer_all.save()
+    try:
+        lookup_str = request.POST['look_up']
+        lookup = request.POST['lookup']
+        return render(request, 'dictionarys/customers.html', look_up_cst(lookup))
+    except:
+        return HttpResponseRedirect(reverse('salesreport:customers') + page_no)
