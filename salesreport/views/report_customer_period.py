cf93a28f5dd2be4e75da543db6b25f54668652b2
Ilya Tsibul <tsigel@vikivostok.ru>
2023-03-13 09:43:00 +0300

    heck for working time — map 1.5s for — 2.6s but we have two maps so 3s in total

--- salesreport/views/report_customer_period.py
+++ salesreport/views/report_customer_period.py
@@ -1,9 +1,11 @@
 import datetime
 from datetime import date
+from functools import partial
 from salesreport.reports import *
 from django.shortcuts import render, HttpResponseRedirect, reverse
 from salesreport.views.service import *
 from salesreport.report_period import ReportPeriod
+
 
 
 def report_customer_period(request):
@@ -16,14 +18,18 @@ def report_customer_period(request):
     date_finish = datetime.datetime.strptime(request.POST['date_end'], '%Y-%m-%d').date()
     period_type = request.POST['period_type']
     periods = ReportPeriod.objects.filter(period=period_type, date_end__gte=date_start,
-                                          date_begin__lte=date_finish).order_by(
-        'date_begin')
+                                          date_begin__lte=date_finish).order_by('date_begin')
     Class = check_class(period_type)
     customers_periods = Class.objects.filter(period__in=periods)
     names = set(customers_periods.values_list('name', flat=True))
+    grand_total_eco = sum(customers_periods.values_list(report_type_eco, flat=True))
+    grand_total_no_eco = sum(customers_periods.values_list(report_type_no_eco, flat=True))
     report_eco = []
     report_no_eco = []
     print(datetime.datetime.now().time())
+#    report_eco = filter(lambda x: x is not None, map(partial(eco_total, Class, periods, report_type_eco), names))
+#    report_no_eco = filter(lambda x: x is not None, map(partial(eco_total, Class, periods, report_type_no_eco), names))
+
     for name in names:
         customers_periods = Class.objects.filter(period__in=periods, name=name)
         total_amount_eco = sum(list(customers_periods.values_list(report_type_eco, flat=True)))
@@ -43,4 +49,13 @@ def report_customer_period(request):
     context = {'navi': navi, 'report_eco': report_eco, 'report_no_eco': report_no_eco, 'periods': periods,
                'period_types': period_types, 'report_type': report_type, 'date_begin': date_start,
                'date_end': date_finish, 'per_type': period_type}
+    print(datetime.datetime.now().time())
     return render(request, 'salesreport/reports/customer_period.html', context)
+
+
+def eco_total(Class, periods, report_type_eco, name):
+    customers_periods = Class.objects.filter(period__in=periods, name=name)
+    total_amount_eco = sum(list(customers_periods.values_list(report_type_eco, flat=True)))
+    if total_amount_eco:
+        return [name, list(customers_periods.values_list(report_type_eco, 'customer', 'period')), total_amount_eco]
+
